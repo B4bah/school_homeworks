e19416d9bdcee8ac89cc8fbdd5ad14624a06ca92
B4bah <122825782+B4bah@users.noreply.github.com>
2023-10-13 16:10:31 +0300

    Update divisors_func.py

--- Funcs/divisors_func.py
+++ Funcs/divisors_func.py
@@ -1,6 +1,6 @@
 def divs(num):
     D = []
-    for i in range(1, int(num**0.5)):
+    for i in range(1, int(num**0.5)+1):
         if num % i == 0:
             D.append(num // i)
             D.append(i)
