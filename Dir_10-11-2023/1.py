bb685dec4e6438491fc9c205489259520b079c26
B4bah <osipovmisha0909@yandex.ru>
2023-11-11 00:13:03 +0300

    New hard task

--- Dir_10-11-2023/1.py
+++ Dir_10-11-2023/1.py
@@ -8,42 +8,34 @@ def mov(t: tuple[int]) -> tuple[tuple[int, int], tuple[int, int], tuple[int, int
 
 
 @lru_cache(None)
-def f(n):
-    if any(sum(x) >= 70 for x in mov(n)):
-        return 1
-    elif all(f_(x) == 1 for x in mov(n)):
-        return -1
-    elif any(f(x) == -1 for x in mov(n)):
-        return 2
-    elif all(f(x) >= 0 for x in mov(n)):
-        return -2
-    else:
-        return 0
+def f(n: int) -> int:
+    if any(sum(x) >= 70 for x in mov(n)): return 1
+    elif all(f_(x) == 1 for x in mov(n)): return -1
+    elif any(f(x) == -1 for x in mov(n)): return 2
+    elif all(f(x) >= 0 for x in mov(n)): return -2
+    else: return 0
 
 
-def f_(n):
-    if any(sum(x) >= 70 for x in mov(n)):
-        return 1
-    elif any(f(x) == 1 for x in mov(n)):
-        return -1
-    else:
-        return 0
+def f_(n: int) -> int:
+    if any(sum(x) >= 70 for x in mov(n)): return 1
+    elif any(f(x) == 1 for x in mov(n)): return -1
+    else: return 0
 
 
 for i in range(1, 63):
-    if f_((6, i)) == -1: print('1', i); break
+    if f_((6, i)) == -1: print('1)', i); break
 
 k = 0
 for i in range(1, 63):
-    if f((6, i)) != 1 and f((6, i)) == 2:
+    if f((6, i)) == 2:
         k += 1
-print('2', k)
+print('2)', k)
 
 for i in range(63, 1, -1):
     if f_((6, i)) == -1:
-        print('3', i)
+        print('3)', i)
         break
 
-# 1 21
-# 2 3
-# 3 21
+# 1) 8
+# 2) 3
+# 3) 21
--- Dir_10-11-2023/8564.py
+++ Dir_10-11-2023/8564.py
@@ -2,12 +2,12 @@ from functools import lru_cache
 
 
 def mov(n: int) -> tuple[int, int, int]:
-    return n+1, n+3, n*4
+    return (n - 3, n - 2, n // 4) if n % 4 == 0 else (n - 3, n - 2)
 
 
 @lru_cache(None)
 def f(n: int) -> int:
-    if any(x >= 59 for x in mov(n)): return 1
+    if any(x <= 31 for x in mov(n)): return 1
     elif all(f(x) == 1 for x in mov(n)): return -1
     elif any(f(x) == -1 for x in mov(n)): return 2
     elif all(f(x) >= 0 for x in mov(n)): return -2
@@ -15,32 +15,33 @@ def f(n: int) -> int:
 
 
 def f_(n: int) -> int:
-    if any(x >= 59 for x in mov(n)): return 1
+    if any(x <= 31 for x in mov(n)): return 1
     elif any(f_(x) == 1 for x in mov(n)): return -1
-    elif all(f_(x) == -1 for x in mov(n)): return 2
+    elif any(f_(x) == -1 for x in mov(n)): return 2
     elif any(f_(x) >= 0 for x in mov(n)): return -2
     else: return 0
 
 
-for i in range(1, 58):
+for i in range(100, 32, -1):
     if f(i) == -1:
         print('1)', i)
         break
 
-print('2) ', end='')
-k = 0
-for i in range(1, 58):
+for i in range(140, 32, -1):
     if f(i) == 2:
-        k += 1
-        print(i, end=' ')
-        if k == 2: break
-print()
+        print('2)', i, end=' ')
+        break
 
-for i in range(1, 58):
-    if f_(i) == -2:
+for i in range(32, 100):
+    if f(i) == 2:
+        print(i)
+        break
+
+for i in range(32, 100):
+    if f(i) == -2:
         print('3)', i)
         break
 
-# 1) 14
-# 2) 11 13
-# 3) 2
+# 1) 35
+# 2) 140 37
+# 3) 39
